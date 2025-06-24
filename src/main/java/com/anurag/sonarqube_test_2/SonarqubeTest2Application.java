8dba38570f77f8cc47546a4e403a9dc9c8fe305b
Anurag-Kumar-Poddar <anuragkumarpoddar736@gmail.com>
2025-06-24 11:52:33 +0530

    add files

---
+++ src/main/java/com/anurag/sonarqube_test_2/SonarqubeTest2Application.java
@@ -0,0 +1,13 @@
+package com.anurag.sonarqube_test_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/anurag/sonarqube_test_2/SonarqubeTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anurag.sonarqube_test_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
